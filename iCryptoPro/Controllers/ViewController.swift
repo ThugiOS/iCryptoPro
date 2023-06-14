e5a0790f5f8f517db7ed8f006315e10aea4f98bb
catthug92 <itnikitins@gmail.com>
2023-06-14 17:02:48 +0300

    create project

---
+++ iCryptoPro/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iCryptoPro
+//
+//  Created by Никитин Артем on 14.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemRed
+    }
+
+
+}
+
---
+++ iCryptoPro/Supporting/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  iCryptoPro
+//
+//  Created by Никитин Артем on 14.06.23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    // MARK: - API
+
+    static let scheme = "https"
+    static let baseURL = "pro-api.coinmarketcap.com"
+    static let port: Int? = nil
+    static let fullURL = "https://pro-api.coinmarketcap.com"
+    
+    static let API_KEY = "d482a6e6-4e7d-4427-af82-1b5a1c57ca65"
+}
